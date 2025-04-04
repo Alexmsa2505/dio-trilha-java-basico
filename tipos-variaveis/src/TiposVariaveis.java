f673f32f4a37874f069b4e359f78ed2d55bff403
Alexmsa2505 <alex.manoel09@gmail.com>
2025-04-04 00:06:13 -0300

    ...

--- tipos-variaveis/src/TiposVariaveis.java
+++
@@ -1,9 +0,0 @@
-public class TiposVariaveis {
-    public static void main(String[] args) throws Exception {
-        
-        String modeloCarro = "Jetta";
-        int anoCarro = 2014;
-        System.out.print( modeloCarro + " ano é de " + anoCarro);
-        
-    }
-}
